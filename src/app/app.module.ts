a74f8762d7f6c9390cf8617dc80c8c8f82a948d1
Brigitta Juhász <brigitta.juhasz@adnovum.hu>
2021-06-27 19:33:13 +0200

    new component for the quiz, button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {HomeComponent} from './home/home.component';
+import {HomeComponent} from './components/home/home.component';
+import { QuestionsComponent } from './components/questions/questions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    QuestionsComponent
   ],
   imports: [
     BrowserModule
